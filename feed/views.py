ae93a271b50b7226be5864188b4fb3d488ed2e48
Lisgevan <episimo@hotmail.com>
2021-05-12 17:38:34 +0300

    Displaying dynamic data

--- feed/views.py
+++ feed/views.py
@@ -4,6 +4,8 @@
 #for class based views
 from django.views.generic import TemplateView
 
+from.models import Post
+
 # Create your views here.
 
 class HomePageView(TemplateView):
@@ -11,5 +13,5 @@ class HomePageView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['my_thing'] = "Hello worlds :P this is dynamic"
+        context['posts'] = Post.objects.all()
         return context
